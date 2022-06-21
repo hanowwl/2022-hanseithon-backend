038bd4f1f4b8a9de3f03fb8e357c84057f951579
HanSeo0507 <i.hansu.lee@gmail.com>
2022-06-21 19:15:19 +0900

    fix: typeorm entities 경로 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
+import * as path from 'path';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -17,17 +19,21 @@ import { AppService } from './app.service';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'mariadb',
-        host: configService.get<string>('TYPEORM_HOST'),
-        port: parseInt(configService.get<string>('TYPEORM_PORT'), 10),
-        username: configService.get<string>('TYPEORM_USERNAME'),
-        password: configService.get<string>('TYPEORM_PASSWORD'),
-        database: configService.get<string>('TYPEORM_DATABASE'),
-        entities: [configService.get<string>('TYPEORM_ENTITIES')],
-        logging: configService.get<string>('TYPEORM_LOGGING') === 'true',
-        synchronize: configService.get<string>('NODE_ENV') === 'development',
-      }),
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'mariadb',
+          host: configService.get<string>('TYPEORM_HOST'),
+          port: parseInt(configService.get<string>('TYPEORM_PORT'), 10),
+          username: configService.get<string>('TYPEORM_USERNAME'),
+          password: configService.get<string>('TYPEORM_PASSWORD'),
+          database: configService.get<string>('TYPEORM_DATABASE'),
+          entities: [
+            path.join(__dirname, configService.get<string>('TYPEORM_ENTITIES')),
+          ],
+          logging: configService.get<string>('TYPEORM_LOGGING') === 'true',
+          synchronize: configService.get<string>('NODE_ENV') === 'development',
+        };
+      },
     }),
   ],
   controllers: [AppController],
