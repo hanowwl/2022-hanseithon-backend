e5c372cd084ef5b85f992276eed4d15d876c5430
HanSeo0507 <i.hansu.lee@gmail.com>
2022-06-21 12:56:18 +0900

    feat: 환경변수 셋업

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === 'production' ? '.env' : '.env.local',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('development', 'production').required(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
