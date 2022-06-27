93fbd8383ce859d544f110408d5d2ab187181c9d
rlaeodnr1011 <skfvkfl1011@gmail.com>
2022-06-27 12:36:00 +0900

    refactor: 모든 유저 정보 가져오기 기능 삭제

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,10 @@
-import { Controller, Get, Ip, Post, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { GetUser } from 'src/auth/decorators';
 import { AccessTokenAuthGuard } from 'src/auth/guards';
 import { User } from 'src/entities';
-import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
-
-  @UseGuards(AccessTokenAuthGuard)
-  @Get('all')
-  async getAllUser() {
-    return await this.usersService.getAllUser();
-  }
-
   @UseGuards(AccessTokenAuthGuard)
   @Get('profile')
   async profile(@GetUser() user: User) {
