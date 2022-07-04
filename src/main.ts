8c7df649eab4962e5d7b7983e8aef584f7f8b47a
HanSeo0507 <i.hansu.lee@gmail.com>
2022-07-04 13:40:18 +0900

    feat: http-exception filter 추가

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './libs/filters';
 import { ResponseInterceptor } from './libs/interceptors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalPipes(
     new ValidationPipe({
