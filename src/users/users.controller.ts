8c9e871591c25496de8b3ddae95e8b59931ac3ea
HanSeo0507 <i.hansu.lee@gmail.com>
2022-07-07 15:55:51 +0900

    fix: @GetUser 데코레이터 경로 수정

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AccessTokenAuthGuard } from 'src/auth/guards';
 import { User } from 'src/entities';
-import { GetUser } from 'src/users/decorators';
+import { GetUser } from './decorators';
 
 @Controller('users')
 export class UsersController {
