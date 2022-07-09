735f8da5c68c2996f18a782a84f633f9e2234798
HanSeo0507 <i.hansu.lee@gmail.com>
2022-07-09 17:14:12 +0900

    feat: team 엔티티 'type' 컬럼 추가, team-member 포지션 타입 수정

--- src/constants/type.ts
+++ src/constants/type.ts
@@ -1,9 +1,10 @@
 export const TEAM_MEMBER_POSITION_VALUES = [
-  'FRONT',
-  'BACK',
-  'FULL-STACK',
+  'DEVELOPER',
   'PM',
   'DESIGN',
 ] as const;
 export type TEAM_MEMBER_POSITION_TYPE =
   typeof TEAM_MEMBER_POSITION_VALUES[number];
+
+export const TEAM_TYPE_VALUES = ['GAME', 'LIVING'];
+export type TEAM_TYPE = typeof TEAM_TYPE_VALUES[number];
