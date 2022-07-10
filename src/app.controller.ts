fe33c046247912539a41b0dc831133ce5e9b5209
rlaeodnr1011 <skfvkfl1011@gmail.com>
2022-07-11 08:37:00 +0900

    feat: time 조회 로직 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CustomDate } from './auth/dto/custom-date.dto';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('time')
+  async getServcerTime(): Promise<CustomDate> {
+    return await this.appService.getServerTime();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,16 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  public async getServerTime() {
+    const today = new Date();
+    return {
+      year: today.getFullYear(),
+      month: today.getMonth() + 1,
+      day: today.getUTCDate() + 1,
+      hour: today.getUTCHours() - 15,
+      minute: today.getUTCMinutes(),
+      second: today.getUTCSeconds(),
+    };
+  }
 }
